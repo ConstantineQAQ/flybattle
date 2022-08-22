351a7941aeddea76a086e0902861ffa16fa45743
ConstantineQAQ <357208746@qq.com>
2022-08-22 18:58:21 +0800

    new file

---
+++ src/Boom.java
@@ -0,0 +1,91 @@
+import java.awt.Graphics;
+
+import javax.swing.ImageIcon;
+
+public class Boom {
+
+    private int x;
+    private int y;
+    private int width;
+    private int height;
+    private ImageIcon boomimg = new ImageIcon("resources/bomb.png");
+    private int count;//删除的次数
+
+    public int getCount() {
+        return count;
+    }
+
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+
+
+    public Boom(int x, int y) {
+        this.x = x;
+        this.y = y;
+        this.width = boomimg.getIconWidth();
+        this.height = boomimg.getIconHeight();
+    }
+
+
+    public int getX() {
+        return x;
+    }
+
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+
+    public int getY() {
+        return y;
+    }
+
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+
+    public int getWidth() {
+        return width;
+    }
+
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+
+    public int getHeight() {
+        return height;
+    }
+
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+
+    public ImageIcon getBombimg() {
+        return boomimg;
+    }
+
+
+    public void setBombimg(ImageIcon bombimg) {
+        this.boomimg = bombimg;
+    }
+
+
+    public void drawImage(Graphics g) {
+        g.drawImage(boomimg.getImage(), x, y, null);
+    }
+
+
+    public void move() {
+        count++;
+    }
+}
+
---
+++ src/Bullet2.java
@@ -0,0 +1,71 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Bullet2 {
+    private int x;
+    private int y;
+    private int width;
+    private int height;
+    private ImageIcon bulletImageIcon = new ImageIcon("resources/bullet.png");
+
+    public Bullet2(int x, int y) {
+        this.x = x;
+        this.y = y;
+        this.width = bulletImageIcon.getIconWidth();
+        this.height = bulletImageIcon.getIconHeight();
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public ImageIcon getBulletImageIcon() {
+        return bulletImageIcon;
+    }
+
+    public void setBulletImageIcon(ImageIcon bulletImageIcon) {
+        this.bulletImageIcon = bulletImageIcon;
+    }
+
+/*    public void move() {
+        this.y -= 4;
+    }*/
+
+    public void move2() {
+        this.y -= 4;
+        this.x -= 4;
+    }
+
+    public void drawImage(Graphics g) {
+        g.drawImage(bulletImageIcon.getImage(), x, y, null);
+
+    }
+}
